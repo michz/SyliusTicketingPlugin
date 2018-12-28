37db41a725464ba8d9002421b334e2e35057a8ca
Michael Zapf <m.zapf@mztx.de>
2018-12-28 17:57:17 +0100

    change namespace

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace mztx\SyliusTicketingPlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -15,7 +15,7 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('acme_sylius_example_plugin');
+        $rootNode = $treeBuilder->root('mztx_sylius_ticketing_plugin');
 
         return $treeBuilder;
     }
--- src/MztxSyliusTicketingPlugin.php
+++ src/MztxSyliusTicketingPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace mztx\SyliusTicketingPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class MztxSyliusTicketingPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
